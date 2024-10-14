8358c037a52464b141001645337838449b639c6e
NapatrSaengthongsakullert <napatr.se@ku.th>
2024-10-14 20:09:16 +0700

    Extract Method for rental price calculation

--- customer.py
+++ customer.py
@@ -23,13 +23,34 @@ class Customer:
     def get_name(self):
         """Get the customer's name."""
         return self.name
-    
+
+    def get_price(self,rental):
+        """Get the rental price for this customer"""
+        amount = 0
+        if rental.get_movie().get_price_code() == Movie.REGULAR:
+            # Two days for $2, additional days 1.50 per day.
+            amount = 2.0
+            if rental.get_days_rented() > 2:
+                amount += 1.5 * (rental.get_days_rented() - 2)
+        elif rental.get_movie().get_price_code() == Movie.CHILDRENS:
+            # Three days for $1.50, additional days 1.50 per day.
+            amount = 1.5
+            if rental.get_days_rented() > 3:
+                amount += 1.5 * (rental.get_days_rented() - 3)
+        elif rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
+            # Straight $3 per day charge
+            amount = 3 * rental.get_days_rented()
+        else:
+            log = logging.getLogger()
+            log.error(f"Movie {rental.get_movie()} has unrecognized priceCode {rental.get_movie().get_price_code()}")
+        return amount
+
     def statement(self):
         """Create a statement of rentals for the current period.
 
-        Print all the rentals in the current period, 
+        Print all the rentals in the current period,
         along with total charges and frequent renter points.
-        
+
         Returns:
             the statement as a String
         """
@@ -40,26 +61,10 @@ class Customer:
         header_fmt = "{:40s}  {:6s} {:6s}\n"
         statement += header_fmt.format("Movie Title", "  Days", " Price")
         rental_fmt = "{:40s}  {:6d} {:6.2f}\n"
-        
+
         for rental in self.rentals:
             # compute rental change
-            amount = 0
-            if rental.get_movie().get_price_code() == Movie.REGULAR:
-                # Two days for $2, additional days 1.50 per day.
-                amount = 2.0
-                if rental.get_days_rented() > 2:
-                    amount += 1.5*(rental.get_days_rented()-2)
-            elif rental.get_movie().get_price_code() == Movie.CHILDRENS:
-                # Three days for $1.50, additional days 1.50 per day.
-                amount = 1.5
-                if rental.get_days_rented() > 3:
-                    amount += 1.5*(rental.get_days_rented()-3)
-            elif rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
-                # Straight $3 per day charge
-                amount = 3*rental.get_days_rented()
-            else:
-                log = logging.getLogger()
-                log.error(f"Movie {rental.get_movie()} has unrecognized priceCode {rental.get_movie().get_price_code()}")
+            amount = self.get_price(rental)
             # compute the frequent renter points based on movie price code
             if rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
                 # New release earns 1 point per day rented
@@ -69,8 +74,8 @@ class Customer:
                 frequent_renter_points += 1
             #  add a detail line to statement
             statement += rental_fmt.format(
-                            rental.get_movie().get_title(), 
-                            rental.get_days_rented(), 
+                            rental.get_movie().get_title(),
+                            rental.get_days_rented(),
                             amount)
             # and accumulate activity
             total_amount += amount
